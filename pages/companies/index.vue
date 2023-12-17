b3c4b31c093521c17b813fbc7f66b359cd7faae5
chandra171 <chandraadhiyasa171@gmail.com>
2023-12-17 22:17:52 +0700

    commit index dan create employee

--- pages/companies/index.vue
+++ pages/companies/index.vue
@@ -1,17 +1,18 @@
-<template>
+<!-- <template>
   <section class="py-[200px] flex flex-col items-center justify-center px-4">
     <div class="text-[32px] font-semibold text-dark mb-4">Select Companies</div>
     <div class="w-full card">
       <div class="form-group">
         <label for="" class="text-grey">Companies</label>
-        <select
-          v-if="companies.data"
-          v-model = 'selectedCompany'
+
+        <p v-if="$fetchState.pending">Fetching companies...</p>
+        <select v-else
+          v-model="selectedCompany"
           name="companies"
           id=""
           class="appearance-none input-field form-icon-chevron_down"
         >
-          <option :value="company.id" v-for="company in companies.data.result.data" selected>
+          <option :value="company.id" v-for="company in companies.data.result.data">
             {{ company.name }}
           </option>
         </select>
@@ -25,30 +26,10 @@
         </NuxtLink>
     </div>
   </section>
-</template>
+</template> -->
 
 <script>
 export default {
   middleware: 'auth',
-  data() {
-    return {
-      companies: [],
-      selectedCompany: '',
-    }
-  },
-  async fetch() {
-    this.companies = await this.$axios.get('company?limit=100')
-  },
-  methods: {
-    openCompany() {
-      this.$router.push({
-        name: 'companies-id',
-        params: {
-          id: this.selectedCompany,
-        },
-      })
-    },
-  }
-
 }
 </script>
